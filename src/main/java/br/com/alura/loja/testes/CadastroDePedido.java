bd9317b52bf399acd98a3d3fa4d19f8b4da8fe7e
Thamyris <tgthurler@gmail.com>
2022-05-30 20:29:42 -0300

    Criado uma consulta para exibir somatório total dos produtos e consertado erro que não persistia a somatória no banco

--- src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -22,38 +22,41 @@ public class CadastroDePedido {
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		ClienteDao clienteDao = new ClienteDao(em);
-		
+
 		Produto produto = produtoDao.buscarPorId(1l);
 		Cliente cliente = clienteDao.buscarPorId(1l);
-		
+
 		em.getTransaction().begin();
-		
+
 		Pedido pedido = new Pedido(cliente);
 		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
-		
+
 		PedidoDao pedidoDao = new PedidoDao(em);
 		pedidoDao.cadastrar(pedido);
-		
+
 		em.getTransaction().commit();
+
+		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
+		System.out.println("Valor Total : "+totalVendido);
 	}
-	
+
 	private static void popularBancoDeDados() {
 		Categoria celulares = new Categoria("CELULARES");
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
-		
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+
 		Cliente cliente = new Cliente("Rodrigo", "123456");
-		
+
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
 		ClienteDao clienteDao = new ClienteDao(em);
-		
+
 		em.getTransaction().begin();
-		
+
 		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
 		clienteDao.cadastrar(cliente);
-		
+
 		em.getTransaction().commit();
 		em.close();
 	}
