f76d7c5a45b0e7f6ea9540422946723ed0938e5e
Thamyris <tgthurler@gmail.com>
2022-05-30 21:08:45 -0300

    Feito um exemplo de sql utlizando Named Queries

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -44,9 +44,15 @@ public class ProdutoDao {
 				.getResultList();
 	}
 	
+//	public List<Produto> buscarPorNomeDaCategoria(String nome) {
+//		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+//		return em.createQuery(jpql, Produto.class)
+//				.setParameter("nome", nome)
+//				.getResultList();
+//	}
+	
 	public List<Produto> buscarPorNomeDaCategoria(String nome) {
-		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
-		return em.createQuery(jpql, Produto.class)
+		return em.createNamedQuery("Produto.produtosPorCategoria", Produto.class)
 				.setParameter("nome", nome)
 				.getResultList();
 	}
